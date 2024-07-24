158f1bf5cd5ef58954a023695627907b7acdd45d
Hodaya Mashraki <mashraki.hodaya@gmail.com>
2024-07-24 14:39:02 +0300

    fixed bug in part 4

--- hw2/cnn.py
+++ hw2/cnn.py
@@ -277,44 +277,10 @@ class ResidualBottleneckBlock(ResidualBlock):
         #  Initialize the base class in the right way to produce the bottleneck block
         #  architecture.
         # ====== YOUR CODE: ======
-        super().__init__(in_channels=in_out_channels, channels=inner_channels, kernel_sizes=inner_kernel_sizes)
-        main_layers=[]
-        shortcut_layaers=[]
-        batchnorm: bool = False
-        dropout: float = 0.0
-        activation_type: str = "relu"
-        activation_params: dict = {}
-        #main path
-        current_in_channels = in_out_channels
-        for i, (out_channels, kernel_size) in enumerate(zip(inner_channels, inner_kernel_sizes)):
-            #first projection- to first inner channel. CHECK IF BIAS =TRUE
-            if (i==0):
-                main_layers.append(nn.Conv2d(current_in_channels, out_channels, kernel_size=1))
-                current_in_channels = out_channels
-            # in order to stay with the same size as the image
-            padding=(kernel_size-1 )//2
-            dilation=1
-            stride=1
-            main_layers.append(nn.Conv2d(current_in_channels, out_channels, kernel_size, bias=True, padding=padding, dilation=dilation, stride=stride))
-            if dropout>0:
-                main_layers.append(nn.Dropout2d(dropout))
-            if batchnorm:
-                main_layers.append(nn.BatchNorm2d(out_channels))
-            if activation_params:
-                main_layers.append(ACTIVATIONS[activation_type](**activation_params))
-            else:
-                main_layers.append(ACTIVATIONS[activation_type]())
-            
-            #last projection back to in_out_channels CHECK IF BIAS =TRUE
-            if (i==len(inner_channels)-1):
-                main_layers.append(nn.Conv2d(out_channels, in_out_channels, kernel_size=1))
-            
-            current_in_channels = out_channels  
-        self.main_path=nn.Sequential(*main_layers)
+        inner_channels= [inner_channels[0], *inner_channels, in_out_channels]
+        inner_kernel_sizes= [1, *inner_kernel_sizes, 1]
+        super().__init__(in_channels=in_out_channels, channels=inner_channels, kernel_sizes=inner_kernel_sizes, **kwargs)
 
-        #shortcut path
-        shortcut_layaers.append(nn.Identity())    
-        self.shortcut_path = nn.Sequential(*shortcut_layaers)
         # ========================
 
 
