76716a2ddffc8c6974312839c4427601874982b9
Hodaya Mashraki <mashraki.hodaya@gmail.com>
2024-07-27 17:22:07 +0300

    same

--- exp1_3.sh
+++ exp1_3.sh
@@ -13,16 +13,14 @@ OUTPUT_DIR="/home/inbar.m/hw/deep_on_gpu_hw2/out"
 # Create the output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
 
-
 # Function to run experiment 1.3
 run_exp_1_3(){
   K=(64 128)
   LS=(2 3 4)
   # Loop through each configuration and submit the experiment
-
   for L in "${LS[@]}"; do
-      RUN_NAME="exp1_3_${L}_K64-128"
-
+      RUN_NAME="exp1_3_L${L}_K64-128"
+      
       sbatch \
         -N $NUM_NODES \
         -c $NUM_CORES \
@@ -32,7 +30,6 @@ run_exp_1_3(){
         --mail-type $MAIL_TYPE \
         -o "${OUTPUT_DIR}/${RUN_NAME}.out" \
         <<EOF
-
 #!/bin/bash
 echo "*** SLURM BATCH JOB '${RUN_NAME}' STARTING ***"
 
@@ -44,14 +41,13 @@ conda activate cs236781-hw2-git
 python -m hw2.experiments run-exp -n "exp1_3" \
     --seed 42 --bs-train 32 --batches 1500 --bs-test 32 \
     --epochs 30 --early-stopping 3 \
-    --filters-per-layer ${K} --layers-per-block ${L} \
-    --pool-every 4 --hidden-dims 128\
+    --filters-per-layer ${K[@]} --layers-per-block ${L} \
+    --pool-every 4 --hidden-dims 128 \
     --lr 1e-4 --reg 1e-4 --model-type cnn \
     --out-dir /home/inbar.m/hw/deep_on_gpu_hw2/results
 
 echo "*** SLURM BATCH JOB '${RUN_NAME}' DONE ***"
 EOF
-      done
   done
 }
 
